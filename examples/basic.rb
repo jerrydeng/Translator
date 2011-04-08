ac237611718e29e578ed8891bafeca0e7eb0d9f2
Jerry Deng <jerrydeng93@gmail.com>
2011-04-07 21:40:52 -0400

    fun addition of space in the query

---
+++ examples/basic.rb
@@ -0,0 +1,5 @@
+require '../lib/translator'
+
+class Intepreter
+  include Translator
+end
--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,8 @@ module Translator
     key = CONFIGURATIONS[:key]
     sr  = CONFIGURATIONS[:source_language] || "en"
     ds  = CONFIGURATIONS[:destination_language] || "zh-TW"
-    response = HTTParty.get('https://www.googleapis.com/language/translate/v2?key='+key+'&q='+string+'&source='+sr+'&target='+ds)
+    query = string.gsub(" ", "%20")
+    response = HTTParty.get('https://www.googleapis.com/language/translate/v2?key='+key+'&q='+query+'&source='+sr+'&target='+ds)
     response["data"]["translations"][0]["translatedText"]
   end
 end
